5f045ee135d6fef013b5d829a253c93b880f6744
Kumkum27-byte <kumkum.morewal27@gmail.com>
2025-10-29 16:05:42 +0530

    added main backend file

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel 
 import pandas as pd
 import pickle
 import os
-
+  
 #creating app instance
-app = FastAPI(title="Movie Recommendation System")
+app = FastAPI()  
 
-#get current directory path
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-#load saved data
-movies_dict = pickle.load(open(os.path.join(BASE_DIR, "movie_dict.pkl"),"rb"))
-similarity = pickle.load(open(os.path.join(BASE_DIR, "similarity.pkl"),"rb"))
-movies = pd.DataFrame(movies_dict)
-#recommendation function
-def recommend(movie):
-    movie_index = [movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movie_dict = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])
+class MovieRequest(BaseModel):
+    movie_name: str  
+     
+def _init_(self, movie_name):     
+    self.movie_name = movie_name   
     
-    recommended_movies = []
-    for i in movies_dict[1:6]:
-        recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+# Get current directory path 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
 
-#get route
-@app.get('/')
-def home():
-    return('message : Welcome to the movie recommendationj API')
+# Load files 
+movie_dict = pickle.load(open('movie_dict.pkl', 'rb')) 
+similarity = pickle.load(open(os.path.join(BASE_DIR, "similarity.pkl"), "rb")) 
+movies = pd.DataFrame(movie_dict)                           
 
-#recommend route
-@app.post('/recommend')
-def recommend_movies(request:MovieRequest):
-    movie_name = request.movie_name
-    recommendations = recommend(movie_name)
-    return{"recommended_movies" : recommendations}
+#recommendation function 
+def recommend(movie):     
+    movie_index = movies[movies['title'] == movie].index[0]     
+    distances = similarity[movie_index]     
+    movie_dict = sorted(list(enumerate(distances)), reverse = True, key=lambda x:x[1])[1:6]      
+    
+    recommended_movies = []     
+    for i in movie_dict:         
+        recommended_movies.append(movies.iloc[i[0]].title)      
+    return recommended_movies  
+
+#get route 
+@app.get('/') 
+def home():     
+    return ('message : Welcome to the Movie recommendation API!')  
+
+#recommend route 
+@app.post('/recommend') 
+def recommend_movies(request:MovieRequest):     
+    movie_name = request.movie_name     
+    recommendations = recommend(movie_name)     
+    return {'recommended_movies': recommendations}
